f26b4563893d75c0417159d3bf837681a8151001
Talamar1 <me@danielchester.com>
2015-06-12 01:25:02 -0400

    Rewrote "on block built" event in new ScriptEvent format. Removed from BukkitWorldScriptHelper.

--- src/main/java/net/aufdemrand/denizen/Denizen.java
+++ src/main/java/net/aufdemrand/denizen/Denizen.java
@@ -578,6 +578,7 @@ public class Denizen extends JavaPlugin implements DenizenImplementation {
             eventManager().registerCoreMembers();
 
             ScriptEvent.registerScriptEvent(new BiomeEnterExitScriptEvent());
+            ScriptEvent.registerScriptEvent(new BlockBuiltScriptEvent());
             ScriptEvent.registerScriptEvent(new BlockBurnsScriptEvent());
             ScriptEvent.registerScriptEvent(new BlockFallsScriptEvent());
             ScriptEvent.registerScriptEvent(new BlockPhysicsScriptEvent());
