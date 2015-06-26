161e611aab5c1d2c0139b5e7ccc86772f581f368
Fortifier42 <fortifier42@theausfort.net>
2015-06-26 12:24:53 +1000

    Update Command Meta Add displayitem command description and usage

--- src/main/java/net/aufdemrand/denizen/scripts/commands/BukkitCommandRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/BukkitCommandRegistry.java
@@ -746,12 +746,24 @@ public class BukkitCommandRegistry extends CommandRegistry {
         // @Author aufdemrand, mcmonkey
         // @Group item
         // @Description
-        // TODO: Document Command Details
+        // This command drops an item at the specified location which cannot be picked up by players.
+        // It accepts a duration which determines how long the item will stay for until disappearing.
+        // If no duration is specified the item will stay for 1 minute, after which the item will disappear.
         // @Tags
         // TODO: Document Command Details
         // <entry[saveName].dropped> returns a dEntity of the spawned item.
         // @Usage
-        // TODO: Document Command Details
+        // Use to display a stone block dropped at a players location
+        // - displayitem stone <player.location>
+        // @Usage
+        // Use to display a diamond sword dropped at 12,64,-847 in world survival
+        // - displayitem diamond_sword 12,64,-847,survival
+        // @Usage
+        // Use to display redstone dust dropped at -54,100,-87 in world creative disappear after 10 seconds
+        // - displayitem redstone -54,100,-87,creative duration:10s
+        // @Usage
+        // Use to save the dropped item to save entry 'item_dropped'
+        // - displayitem redstone -54,100,-87,creative duration:10s save:item_dropped
         // -->
         registerCoreMember(DisplayItemCommand.class,
                 "DISPLAYITEM", "displayitem [<item>] [<location>] (duration:<value>)", 2);
