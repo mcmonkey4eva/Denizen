162bc4c6e1dcd3d9d8349157e82d671480e29009
BloodEko <2sicherheit@gmx.de>
2018-03-09 04:02:40 +0100

    disengage cmd
    for player based engages

--- plugin/src/main/java/net/aufdemrand/denizen/scripts/commands/npc/DisengageCommand.java
+++ plugin/src/main/java/net/aufdemrand/denizen/scripts/commands/npc/DisengageCommand.java
@@ -1,9 +1,12 @@
 package net.aufdemrand.denizen.scripts.commands.npc;
 
 import net.aufdemrand.denizen.BukkitScriptEntryData;
+import net.aufdemrand.denizen.objects.dPlayer;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.aufdemrand.denizencore.exceptions.CommandExecutionException;
 import net.aufdemrand.denizencore.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizencore.objects.Element;
+import net.aufdemrand.denizencore.objects.aH;
 import net.aufdemrand.denizencore.scripts.ScriptEntry;
 import net.aufdemrand.denizencore.scripts.commands.AbstractCommand;
 
@@ -20,8 +23,21 @@ public class DisengageCommand extends AbstractCommand {
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
 
         // Make sure NPC is available
-        if (((BukkitScriptEntryData) scriptEntry.entryData).getNPC() == null) {
+        if (((BukkitScriptEntryData) scriptEntry.entryData).hasNPC()) {
             throw new InvalidArgumentsException("This command requires a linked NPC!");
+        }
+        
+        // Parse arguments
+        for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
+	        if (!scriptEntry.hasObject("player") 
+	        		&& ((BukkitScriptEntryData) scriptEntry.entryData).hasPlayer()
+	        		&& arg.matches("player")) {
+	            scriptEntry.addObject("player", new Element(true) );
+	        }
+	        
+            else {
+                arg.reportUnhandled();
+            }
         }
 
     }
@@ -29,11 +45,21 @@ public class DisengageCommand extends AbstractCommand {
     @Override
     public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
 
-        // Report to dB
-        dB.report(scriptEntry, getName(),
-                ((BukkitScriptEntryData) scriptEntry.entryData).getNPC().debug());
-
-        // Set Disengaged
-        EngageCommand.setEngaged(((BukkitScriptEntryData) scriptEntry.entryData).getNPC().getCitizen(), false);
+    	if (scriptEntry.hasObject("player")) {
+	        dPlayer player = ((BukkitScriptEntryData) scriptEntry.entryData).getPlayer();
+	        dB.report(scriptEntry, getName(),
+	                ((BukkitScriptEntryData) scriptEntry.entryData).getNPC().debug() + player.debug() );
+	
+	        // Set Disengaged
+	        EngageCommand.setEngaged(((BukkitScriptEntryData) scriptEntry.entryData).getNPC().getCitizen(), player, false);
+    	} 
+    	
+    	else {
+	        dB.report(scriptEntry, getName(),
+	                ((BukkitScriptEntryData) scriptEntry.entryData).getNPC().debug());
+	
+	        // Set Disengaged
+	        EngageCommand.setEngaged(((BukkitScriptEntryData) scriptEntry.entryData).getNPC().getCitizen(), false);
+    	}
     }
 }
